36f8d159fdda2fd3f26869143f10f598e4493e1a
Raihan Ahmed <raihan.ahmed117@gmail.com>
2022-05-05 02:10:55 -0500

    converted table name to snake case

--- src/Main.java
+++ src/Main.java
@@ -22,28 +22,28 @@ public class Main {
             String contents = inputData.toLowerCase();
 
             if (contents.contains("create")) {
-                String tname = inputData.split(" ")[2];
+                String t_name = inputData.split(" ")[2];
                 String columns = parse(inputData);
 
-                if (database.get(tname) != null)
+                if (database.get(t_name) != null)
                     System.out.println("Sorry, this table name already exists");
-                else database.add(new Table(tname, columns));
+                else database.add(new Table(t_name, columns));
             }
             else if (contents.contains("insert")) {
-                String tname = inputData.trim().split(" ")[2];
+                String t_name = inputData.trim().split(" ")[2];
                 String[] inputs = parse(inputData).split(",");
 
-                database.get(tname).insert(inputs);
+                database.get(t_name).insert(inputs);
             }
             else if (contents.contains("select")) {
-                String tname = inputData.split(" ")[3];
+                String t_name = inputData.split(" ")[3];
                 String[] vars = inputData.split(" ")[1].split(",");
 
-                System.out.println(database.get(tname).select(vars));
+                System.out.println(database.get(t_name).select(vars));
             }
             else if (contents.contains("drop")) {
-                String tname = inputData.split(" ")[2];
-                database.drop(database.get(tname));
+                String t_name = inputData.split(" ")[2];
+                database.drop(database.get(t_name));
             }else {System.out.println("Command not supported.");}
 
         }
